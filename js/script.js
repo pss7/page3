2bd570f74b9b254fafb87588cbed84544daaf5cf
pss7 <124497122+pss7@users.noreply.github.com>
2023-06-09 11:46:00 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -13,16 +13,43 @@ $(function () {
 
     $('.com_check_box input + .agree_chk').click(function () {
 
-        if($('.com_btn2').hasClass('disabled')){
+        if ($('.com_btn2').hasClass('disabled')) {
             $('.com_btn2').removeClass('disabled');
             $('.com_btn2').addClass('enable');
 
-        }else{
+        } else {
             $('.com_btn2').addClass('disabled');
             $('.com_btn2').removeClass('enable');
         }
-   
+
     });
+
+    /* 해지 */
+    $('.membership_info').click(function () {
+        if ($(this).hasClass('active')) {
+            $(this).removeClass('active');
+            $(this).next().children('p').fadeOut();
+        } else {
+            $(this).addClass('active');
+            $(this).next().children('p').fadeIn();
+        }
+    });
+    $('.membership_info_box .close').click(function () {
+        $('.membership_wrap p').fadeOut();
+        $('.membership_wrap .membership_info').removeClass('active');
+    });
+
+    /* 결제수단 변경 */
+    $('.payment_method_list li a').click(function () {
+        if ($(this).hasClass('active')) {
+            $(this).removeClass('active');
+        } else {
+            $('.payment_method_list li a').removeClass('active');
+            $(this).addClass('active');
+
+        }
+    });
+
 
 
 });
