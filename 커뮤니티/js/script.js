5bade04d949cb5123744b5d67f6146902ded01fa
pss7 <124497122+pss7@users.noreply.github.com>
2023-06-14 02:23:46 +0900

    Add files via upload

---
+++ 커뮤니티/js/script.js
@@ -0,0 +1,68 @@
+$(function () {
+
+    /* aos */
+    $(window).on('load', function () {
+        AOS.init({
+            duration: 1500
+        });
+    });
+
+    /* section1 */
+    function count() {
+        this.el.querySelector(".swiper-counter").innerHTML = '<span class="count">' + (this.realIndex + 1) + '</span>/<span class="total">' + this.el.slidesQuantity + "</span>";
+    }
+
+    $("#section1 .swiper-container").each(function () {
+        this.slidesQuantity = this.querySelectorAll(".swiper-slide").length;
+        new Swiper(this, {
+            slidesPerView: 1,
+            slidesPerGroup: 1,
+            centeredSlides: true,
+            speed: 800,
+            loop: false,
+            navigation: {
+                nextEl: '#section1 .swiper-button-next',
+                prevEl: '#section1 .swiper-button-prev',
+            },
+            on: {
+                init: count,
+                slideChange: count
+            }
+        });
+    });
+
+    /* article1 */
+    $('.tab_content_wrap .tab_content').hide();
+    $('.tab_content_wrap .tab_content').first().show();
+    $('#article1 .tab li').click(function () {
+        $('#article1 .tab li').children().removeClass();
+        $(this).children().addClass('active');
+        var Idx = $(this).index();
+        $('.tab_content_wrap .tab_content').hide();
+        $('.tab_content_wrap .tab_content').eq(Idx).show();
+        return false;
+    });
+
+    /* aside */
+    $('aside .box .branch > a').click(function () {
+        if ($(this).hasClass('active')) {
+            $(this).removeClass('active');
+            $(this).parents('.branch').children('.branch_list').slideUp();
+        } else {
+            $(this).addClass('active');
+            $(this).parents('.branch').children('.branch_list').slideDown();
+
+        }
+        return false;
+    });
+
+    /* top */
+    $('.top_btn').click(function () {
+
+        $('html, body').animate({ scrollTop: 0 },
+            300);
+
+    })
+
+
+});
