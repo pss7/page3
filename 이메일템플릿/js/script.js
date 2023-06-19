3d9a33926899ca3b8399299d8b93a9005a578027
pss7 <124497122+pss7@users.noreply.github.com>
2023-06-20 03:33:52 +0900

    Add files via upload

---
+++ 이메일템플릿/js/script.js
@@ -0,0 +1,12 @@
+$(function () {
+
+    /* aos */
+    $(window).on('load', function () {
+        AOS.init({
+            duration: 1500
+        });
+        $('.main_img').addClass('active');
+    });
+
+
+});
